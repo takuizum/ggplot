907802f489514f5d3fe7dba14933e4738159fd9a
takuizum@cloud <sep10.taku.izum@gmail.com>
2019-07-26 17:58:39 +0000

    add setup file

---
+++ R/00_setup_pkg.R
@@ -0,0 +1,8 @@
+pkg <- c("tidyverse", "broom", "coefplot", "cowplot",
+         "gapminder", "GGally", "ggrepel", "ggridges", "gridExtra", 
+         "here", "interplot", "margins", "maps", "mapproj", 
+         "mapdata", "MASS", "quantreg", "rlang", "scales", 
+         "survey", "suvyr", "viridis", "viridisLite", "devtools")
+install.packages(pkg)
+
+devtools::install_github("kjhealy/socviz")
