62d4a92184c0059bced0466eef3c6e1e480489f1
takuizum@cloud <sep10.taku.izum@gmail.com>
2019-07-28 13:40:21 +0000

    add summarize data

--- R/Chapter5/box_and_jitter.R
+++ R/Chapter5/box_and_jitter.R
@@ -39,3 +39,26 @@ organdata %>% ggplot(aes(x = reorder(country, donors, na.rm = TRUE),
                                          seed = 1234)) + # set random seed
   coord_flip() + labs(x = NULL) +
   theme(legend.position = "top")
+
+# summarize
+by_country <- organdata %>% group_by(consent_law, country) %>%
+  summarise_if(is.numeric, 
+               # funs() is soft deprecated, so use a list(tibble::lst) of either functions or lamdas
+               .funs = lst(mean,# Set list name(ex, mean = mean) when using list not lst.
+                           sd
+                           # ~sd(., na.rm = TRUE) # lambda method(but colnames is not sophisicated.)
+                           ), na.rm = TRUE
+               )
+
+by_country %>% 
+  ungroup %>% # as a plain tibble
+  ggplot(aes(x = donors_mean, y = reorder(country, donors_mean), colour = consent_law)) +
+  geom_point(size = 3)+
+  theme(legend.position = "top")
+
+# geom_ pointrange, a cleveland-style
+# plot a point of mean and specified range.
+by_country %>% ggplot(aes(y = donors_mean, x = reorder(country, donors_mean), colour = consent_law)) + 
+  geom_pointrange(aes(ymin = donors_mean - donors_sd, 
+                      ymax = donors_mean + donors_sd)) + 
+  coord_flip()
